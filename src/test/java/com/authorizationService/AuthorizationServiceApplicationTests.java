15ecce2e2e838bcdbe0cdec14cd0c27417207d3e
MANISH <ms7644030@gmail.com>
2022-08-21 20:14:57 +0530

    modified

--- src/test/java/com/authorizationService/AuthorizationServiceApplicationTests.java
+++ src/test/java/com/authorizationService/AuthorizationServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.authorization_service;
+package com.authorizationService;
 
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AuthorizationServiceApplicationTests {
 
-	// @Test
-	// void contextLoads() {
-	// }
+	@Test
+	void contextLoads() {
+	}
 
 }
