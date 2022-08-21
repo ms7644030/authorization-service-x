fa6910d9c20d53128def86c1b5a92857d53f659a
MANISH <ms7644030@gmail.com>
2022-08-21 20:01:27 +0530

    method contextLoads commented

--- src/test/java/com/authorization_service/AuthorizationServiceApplicationTests.java
+++ src/test/java/com/authorization_service/AuthorizationServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AuthorizationServiceApplicationTests {
 
 	// @Test
-	void contextLoads() {
-	}
+	// void contextLoads() {
+	// }
 
 }
