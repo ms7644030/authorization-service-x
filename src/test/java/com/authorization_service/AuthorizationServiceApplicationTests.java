ffd726b5ea4ab97b3eb1f96a451c62b92a56ad67
MANISH <ms7644030@gmail.com>
2022-08-21 19:55:48 +0530

    @Test commented

--- src/test/java/com/authorization_service/AuthorizationServiceApplicationTests.java
+++ src/test/java/com/authorization_service/AuthorizationServiceApplicationTests.java
@@ -1,12 +1,12 @@
 package com.authorization_service;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AuthorizationServiceApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
